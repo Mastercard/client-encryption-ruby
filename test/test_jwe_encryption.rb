0a3ad40183d863e942a6180e3df752f97458d059
talha-API <123556329+talha-API@users.noreply.github.com>
2023-03-20 12:52:49 +0000

    Test cases for A128/192GCM decryption

--- test/test_jwe_encryption.rb
+++ test/test_jwe_encryption.rb
@@ -92,6 +92,22 @@ class TestJweEncryption < Minitest::Test
     assert_equal decrypted['body']['mapping']['customer_identifier'], 'CUST_12345'
     assert !decrypted['body']['encrypted_data']
   end
+  
+  def test_decrypt_a128gcm
+    resp = File.read('./test/mock/jwe-response-a128gcm.json')
+    jwe = McAPI::Encryption::JweEncryption.new(@test_config)
+    decrypted = JSON.parse(jwe.decrypt(resp))
+    assert_equal decrypted['body']['mapping']['customer_identifier'], 'CUST_12345'
+    assert !decrypted['body']['encrypted_data']
+  end
+
+  def test_decrypt_a192gcm
+    resp = File.read('./test/mock/jwe-response-a192gcm.json')
+    jwe = McAPI::Encryption::JweEncryption.new(@test_config)
+    decrypted = JSON.parse(jwe.decrypt(resp))
+    assert_equal decrypted['body']['mapping']['customer_identifier'], 'CUST_12345'
+    assert !decrypted['body']['encrypted_data']
+  end
 
   def test_decrypt_cbc
     resp = File.read('./test/mock/jwe-response-cbc.json')
